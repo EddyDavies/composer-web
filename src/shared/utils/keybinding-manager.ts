cc82ee9048d04c23c5af8136431798da61978148
eddydavies <eddy@fortick.io>
2025-04-22 14:22:20 +0100

    bug: keybindings wouldn't run with composer focused

--- src/shared/utils/keybinding-manager.ts
+++ src/shared/utils/keybinding-manager.ts
@@ -186,7 +186,7 @@ export class KeybindingManager {
             keybindingEntries.push({
               key: customKey,
               command: kb.command,
-              when: "editorTextFocus",
+              // Don't restrict to editorTextFocus so keybindings work everywhere
             });
           }
         });
@@ -201,7 +201,7 @@ export class KeybindingManager {
             keybindingEntries.push({
               key: defaultKey,
               command: kb.command,
-              when: "editorTextFocus",
+              // Don't restrict to editorTextFocus so keybindings work everywhere
             });
           }
         }
